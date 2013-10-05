2a71528d48acf96445686015c5806a0f7313ea60
Grant Rygh <grantrygh@gmail.com>
2013-10-04 19:49:41 -0500

    Added missing static files.

--- apchemhelp/settings.py
+++ apchemhelp/settings.py
@@ -12,7 +12,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '/home/drew/SoftPackages/chem.db',                      # Or path to database file if using sqlite3.
+        'NAME': os.getenv('SQLITE_DB_PATH'),                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': '',
         'PASSWORD': '',
