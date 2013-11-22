683e0d5a85d6869ecc6ed6693884f3138c80dd17
Drew Rygh <drew.rygh@gmail.com>
2013-11-22 16:28:30 -0600

    Deployment script tweak and updated requirements.

--- fabfile.py
+++ fabfile.py
@@ -1,11 +1,17 @@
 import os
-import sys
 
 from fabric.api import local
+
 
 def deploy():
     print "Deploying APChemHelp branch 'master'."
     print "---> Updating static files..."
-    local('cd assets && rm -rf * && cd ..')
-    local('python manage.py collectstatic')
-    local('git push heroku master')
+    try:
+        os.environ['LOCAL_DEVELOPMENT'] = 'false'
+        local('cd assets && rm -rf * && cd ..')
+        local('python manage.py collectstatic')
+        local('git push heroku master')
+    except:
+        print "Error while deploying."
+
+    os.environ['LOCAL_DEVELOPMENT'] = 'true'
